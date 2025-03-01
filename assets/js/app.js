14e3d4a579513c751ebb311ece77a2f6ca26b14c
unknown <zz240614@gmail.com>
2025-03-01 18:30:23 +0000

    Created function 4 mexedCards and displayed card data

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@ async function fetchCard()
     try{
         let res = await fetch('card.json');
         let data = await res.json();
+        const mixedCards = mixCard(data);
+        createGameBoard(mixedCards);
 
     }catch(error)
     {
@@ -10,4 +12,42 @@ async function fetchCard()
     }
     
 }
+fetchCard();
+
+function mixCard(array)
+{
+    for ( let i =array.length -1; i > 0 ;i--)
+        {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+}
+
+function createGameBoard(cards) {
+    const gameBoard = document.getElementById('gameBoard');
+    gameBoard.innerHTML = '';
+    cards.forEach((card, index) => {  
+        const cardElement = createCard(card, index);
+        gameBoard.appendChild(cardElement);
+    });
+}
+
+function createCard(card, index)
+{
+    const cardElement = document.createElement('div');
+    cardElement.className = 'memory-card';
+    cardElement.innerHTML = `
+        <div class="card-inner" data-card-index="${index}">
+            <div class="card-front">
+                <img src="${card.image}" alt="${card.name}">
+            </div>
+            <div class="card-back">
+                <span>Am here</span>
+            </div>
+        </div>
+    `;
+    return cardElement;
+}
+
 fetchCard();
