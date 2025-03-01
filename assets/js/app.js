562f87876278dea50b6acfd679535244b0a96417
unknown <zz240614@gmail.com>
2025-03-01 21:18:17 +0000

    fixed card matching logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,40 @@
 let lockBoard = false;
-let firstCard = null ; 
-let secondCard = null ; 
+let firstCard = null;
+let secondCard = null;
 
 
-async function fetchCard()
-{   
-    try{
+async function fetchCard() {
+    try {
         let res = await fetch('card.json');
         let data = await res.json();
         const mixedCards = mixCard(data);
         createGameBoard(mixedCards);
 
-    }catch(error)
-    {
+    } catch (error) {
         console.error('Error fetching data:', error);
     }
-    
+
 }
 fetchCard();
 
-function mixCard(array)
-{
-    for ( let i =array.length -1; i > 0 ;i--)
-        {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
+function mixCard(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
 function createGameBoard(cards) {
     const gameBoard = document.getElementById('gameBoard');
     gameBoard.innerHTML = '';
-    cards.forEach((card, index) => {  
+    cards.forEach((card, index) => {
         const cardElement = createCard(card, index);
         gameBoard.appendChild(cardElement);
     });
 }
 
-function createCard(card, index)
-{
+function createCard(card, index) {
     const cardElement = document.createElement('div');
     cardElement.className = 'memory-card';
     cardElement.innerHTML = `
@@ -60,7 +55,7 @@ fetchCard();
 
 function handleCardClick(event) {
     if (lockBoard) return;
-    
+
     const cardInner = event.target.closest('.card-inner');
     if (!cardInner || cardInner.classList.contains('is-flipped')) return;
 
@@ -80,6 +75,25 @@ function flipCard(card) {
 
 function checkForMatch() {
     lockBoard = true;
+    const isMatch = firstCard.querySelector('.card-front span').textContent ===
+        secondCard.querySelector('.card-front span').textContent;
 
+    isMatch ? handleMatch() : handleNotmatch();
 }
+
+function handleMatch() {
+    firstCard.removeEventListener('click', handleCardClick);
+    secondCard.removeEventListener('click', handleCardClick);
+    resetTurn();
+}
+function handleNotmatch() {
+    firstCard.classList.remove('is-flipped');
+    secondCard.classList.remove('is-flipped');
+    resetTurn();
+}
+function resetTurn() {
+    [firstCard, secondCard] = [null, null];
+    lockBoard = false;
+}
+
 
